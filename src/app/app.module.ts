e0be0e85d9b00f5c0dffe21219c96af1eb2b5aac
Joseph Ironsides <davidjosephironsidesmichael@gmail.com>
2022-12-08 17:50:23 -0500

    added logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './sandbox/home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,7 +12,8 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent],
