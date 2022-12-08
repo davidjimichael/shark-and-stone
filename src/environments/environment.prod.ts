7584f45d9a4fb8f9ba3bb0c985c68e759ba85767
Joseph Ironsides <davidjosephironsidesmichael@gmail.com>
2022-12-08 18:50:06 -0500

    init code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'shark-and-stone',
+    appId: '1:241190659185:web:57957e8a6619bc2b06a7d0',
+    databaseURL: 'https://shark-and-stone-default-rtdb.firebaseio.com',
+    storageBucket: 'shark-and-stone.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCuf0UFZBI4RTWSmzD6GW8eyZJAdNlR3Rg',
+    authDomain: 'shark-and-stone.firebaseapp.com',
+    messagingSenderId: '241190659185',
+    measurementId: 'G-KWWECZDW4W',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'shark-and-stone',
+    appId: '1:241190659185:web:57957e8a6619bc2b06a7d0',
+    databaseURL: 'https://shark-and-stone-default-rtdb.firebaseio.com',
+    storageBucket: 'shark-and-stone.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCuf0UFZBI4RTWSmzD6GW8eyZJAdNlR3Rg',
+    authDomain: 'shark-and-stone.firebaseapp.com',
+    messagingSenderId: '241190659185',
+    measurementId: 'G-KWWECZDW4W',
+  },
   production: false
 };
 
